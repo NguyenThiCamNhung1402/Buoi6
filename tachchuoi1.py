2aea5bf529ce2fa37ca45bb7a59b8d7885bb9f12
NguyenThiCamNhung1402 <camnhung140261@gmail.com>
2024-08-20 20:11:32 +0700

    Add files via upload

---
+++ tachchuoi1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 18:37:06 2024
+
+@author: Nguyễn Thị Cẩm Nhung 23712471   
+"""
+
+chuoi =("Đại học Quốc gia, Khu phố 6, P. Linh Trung, Q. Thủ Đức, Tp. HCM")
+sub_string =[s.strip() for s in chuoi.split(',')]
+for s in sub_string:
+    print(s)
+
+
---
+++ toilameo.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 19:37:48 2024
+
+@author: Nguyễn Thị Cẩm Nhung 23712471 
+"""
+c=("i'm a cat")
+c1=c.title()
+print(c1)
+print(c.upper())
+print(c1.swapcase())
+print(c.replace("i","I"))
+print ("Hoặc")
+print(c[0:2 ].upper()+c[2:]) 
